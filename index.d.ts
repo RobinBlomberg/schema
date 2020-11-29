146b6e12e4465b7adcb00b50ee3774a85f213873
Robin <hello@robinblomberg.se>
2020-11-29 22:53:01 +0100

    Use exports instead of declares in .d.ts files

--- index.d.ts
+++ index.d.ts
@@ -6,19 +6,19 @@ import { SchemaValidationError } from './src/SchemaValidationError.js';
 import { StringLiteralSchema } from './src/StringLiteralSchema.js';
 import { StringSchema } from './src/StringSchema.js';
 
-declare function array(items: Schema | Schema[]): ArraySchema;
+export function array(items: Schema | Schema[]): ArraySchema;
 
-declare function number(): NumberSchema;
+export function number(): NumberSchema;
 
-declare function numberLiteral(value: number): NumberLiteralSchema;
+export function numberLiteral(value: number): NumberLiteralSchema;
 
-declare function object(properties: Record<string, Schema>): ObjectSchema;
+export function object(properties: Record<string, Schema>): ObjectSchema;
 
-declare function string(): string;
+export function string(): string;
 
-declare function stringLiteral(value: string): StringLiteralSchema;
+export function stringLiteral(value: string): StringLiteralSchema;
 
-declare function infer(value: any): Schema;
+export function infer(value: any): Schema;
 
 export type Schema =
   | ArraySchema
