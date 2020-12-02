011e8e4e337d2f5317a06c4c4d1eaa3afbbdd138
Robin <hello@robinblomberg.se>
2020-12-02 23:14:03 +0100

    Export Schema type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 export { infer } from "./src/infer.js";
+export type Schema = import("./src/types.js").Schema;
 export { array, numberLiteral, object, string, stringLiteral } from "./src/factory.js";
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+/**
+ * @typedef {import('./src/types').Schema} Schema
+ */
+
 export { array, numberLiteral, object, string, stringLiteral } from './src/factory.js';
 export { infer } from './src/infer.js';
